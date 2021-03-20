2a0850b35e8f182ed3f7a96f47724dd4158e7e57
Szymon Czichy <szymon.czichy@gmail.com>
2021-03-20 22:55:05 +0100

    ng added

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+    int a;
+
+    public Square(int a){
+        this.a = a;
+    }
+
+    public int area (){
+        int area = a*a;
+        return area;
+    }
+
+}
+
+
