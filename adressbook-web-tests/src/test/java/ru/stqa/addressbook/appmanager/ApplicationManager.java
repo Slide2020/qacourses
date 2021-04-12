46881c7ae9cc9b2ab2b572cc7b217b935626b20c
Szymon Czichy <szymon.czichy@gmail.com>
2021-04-12 19:53:09 +0200

    half

--- adressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
@@ -1,12 +1,15 @@
 package ru.stqa.addressbook.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager {
+public class ApplicationManager extends ContactHelper {
 
     FirefoxDriver wd;
+
 
     private  SessionHelper sessionHelper;
     private  NavigationHelper navigationHelper;
@@ -36,5 +39,35 @@ public class ApplicationManager {
 
     public NavigationHelper getNavigationHelper() {
         return navigationHelper;
+    }
+
+    public void goToHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    public void submitContactCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getAdress());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getMail());
+    }
+
+    public void initContactCreation() {
+        wd.findElement(By.linkText("nowy wpis")).click();
     }
 }
--- adressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -1,12 +1,16 @@
 package ru.stqa.addressbook.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.addressbook.appmanager.ApplicationManager;
+import ru.stqa.addressbook.model.ContactData;
 
 public class TestBase {
 
     protected final ApplicationManager app = new ApplicationManager();
+
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -18,5 +22,6 @@ public class TestBase {
     public void tearDown() {
         app.stop();
     }
+
 
 }
